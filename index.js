59b635a6028b1bb4f3308aefecd464dcf1bddf5c
Parsha Pourkhomami <parshap+git@gmail.com>
2014-12-04 01:06:01 -0800

    Initial implementation

---
+++ index.js
@@ -0,0 +1,63 @@
+// jshint camelcase:false
+"use strict";
+
+// ## Load Store
+//
+
+var load = require("./lib/load");
+var Store = require("./lib/store");
+
+function loadStore(dataPath, callback) {
+  var store = new Store();
+  load(store, dataPath, function(err) {
+    if (err) {
+      return callback(err);
+    }
+    callback(null, store);
+  });
+}
+
+// ## Exports
+//
+// A wrapper around lib/schedule.js which includes loading data, calculating
+// the next scheduled stop, and attaching additional related data.
+//
+
+var getSchedule = require("./lib/schedule");
+var getTripStops = require("./lib/trip-stops");
+
+module.exports = function(callback) {
+  loadStore("./data", function(err, store) {
+    if (err) {
+      return callback(err);
+    }
+    callback(null, getScheduleWrapper.bind(null, store));
+  });
+};
+
+function getScheduleWrapper(store, opts) {
+  var getNextStop = getSchedule(store, opts);
+  return function() {
+    var stop = getNextStop();
+    return {
+      fromStop: store.stops[opts.from],
+      toStop: store.stops[opts.to],
+      tripStops: getTripStops(store, stop, opts.from, opts.to),
+      serviceDate: stop.serviceDate,
+      trip: store.trips[stop.stopTime.trip_id],
+      route: getRouteFromStopTime(store, stop.stopTime),
+      service: getServiceFromStopTime(store, stop.stopTime),
+      agency: store.agency,
+    };
+  };
+}
+
+function getRouteFromStopTime(store, stopTime) {
+  var trip = store.trips[stopTime.trip_id];
+  return store.routes[trip.route_id];
+}
+
+function getServiceFromStopTime(store, stopTime) {
+  var trip = store.trips[stopTime.trip_id];
+  return store.calendar[trip.service_id];
+}
---
+++ test.js
@@ -0,0 +1,59 @@
+// jshint camelcase:false
+"use strict";
+
+var test = require("tape");
+var loadCaltrainSchedule = require("./");
+var isSameStopLocation = require("./lib/util").isSameStopLocation;
+
+test("caltrain", function(t) {
+  loadCaltrainSchedule(function(err, getSchedule) {
+    t.ifError(err);
+    var getNextStop = getSchedule({
+      from: "ctsf",
+      to: "ctsmat",
+      date: new Date(2014, 11, 1, 12),
+    });
+    var stop = getNextStop();
+
+    t.equal(stop.serviceDate.getFullYear(), 2014);
+    t.equal(stop.serviceDate.getMonth(), 11);
+    t.equal(stop.serviceDate.getDay(), 1);
+
+    t.equal(stop.trip.route_id, stop.route.route_id);
+    t.equal(stop.trip.service_id, stop.service.service_id);
+
+    var firstTripStop = stop.tripStops[0];
+    var lastTripStop = stop.tripStops[stop.tripStops.length - 1];
+
+    t.equal(stop.trip.trip_id, firstTripStop.stopTime.trip_id);
+
+    t.equal(firstTripStop.stopTime.stop_id, "70012");
+
+    t.equal(firstTripStop.date.getFullYear(), 2014);
+    t.equal(firstTripStop.date.getMonth(), 11);
+    t.equal(firstTripStop.date.getDay(), 1);
+    t.equal(firstTripStop.date.getHours(), 12);
+    t.equal(firstTripStop.date.getMinutes(), 7);
+
+    t.assert(isSameStopLocation(firstTripStop.stop, stop.fromStop));
+    t.assert(isSameStopLocation(lastTripStop.stop, stop.toStop));
+
+    // Assert trip of stop times
+    stop.tripStops.forEach(function(tripStop) {
+      t.equal(tripStop.stopTime.trip_id, stop.trip.trip_id);
+    });
+
+    // Assert and station are same
+    stop.tripStops.forEach(function(tripStop) {
+      t.assert(isSameStopLocation(tripStop.stop, tripStop.station));
+    });
+
+    // Assert order of dates
+    stop.tripStops.slice(1).forEach(function(tripStop, index) {
+      var prevTripStop = stop.tripStops[index];
+      t.ok(tripStop.date >= prevTripStop.date);
+    });
+
+    t.end();
+  });
+});
