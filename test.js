f4e5771b442253c29adb70fcbcb9e9248292f5af
Parsha Pourkhomami <parshap+git@gmail.com>
2016-01-31 22:51:31 -0800

    Improve tests

--- test.js
+++ test.js
@@ -5,10 +5,25 @@ var test = require("tape");
 var CURRENT_YEAR = new Date().getFullYear();
 var loadCaltrainSchedule = require("./async");
 var isStopAtStation = require("./lib/util").isStopAtStation;
+var stations = require("./stations");
 
-test("caltrain", function(t) {
-  loadCaltrainSchedule(function(err, getSchedule) {
-    t.ifError(err);
+loadCaltrainSchedule(function(err, getSchedule) {
+  if (err) {
+    throw err;
+  }
+
+  test("stations", function(t) {
+    stations.forEach(function(station) {
+      t.ok(station);
+      t.ok(station.id);
+      t.ok(station.name);
+      t.equal(stations.byId(station.id), station);
+      t.equal(stations.search(station.name), station);
+    });
+    t.end();
+  });
+
+  test("caltrain", function(t) {
     var getNextStop = getSchedule({
       from: "ctsf",
       to: "ctsmat",
@@ -16,6 +31,7 @@ test("caltrain", function(t) {
     });
     var stop = getNextStop();
 
+    t.assert(stop);
     t.equal(stop.serviceDate.getFullYear(), CURRENT_YEAR);
     t.equal(stop.serviceDate.getMonth(), 11);
     t.equal(stop.serviceDate.getDate(), 1);
@@ -55,6 +71,85 @@ test("caltrain", function(t) {
       t.ok(tripStop.date >= prevTripStop.date);
     });
 
+    t.end();
+  });
+
+  test("get multiple route types", function(t) {
+    var getNextStop = getSchedule({
+      from: "ctsf",
+      to: "ctsj",
+      date: new Date(CURRENT_YEAR, 11, 1, 12),
+    });
+
+    var seenRoutes = new Set();
+    for (var i = 0; i < 20; i++) {
+      var stop = getNextStop();
+      seenRoutes.add(stop.route.route_id);
+    }
+    // Should have seen local, limited, bullet
+    t.equal(seenRoutes.size, 3);
+    t.ok(seenRoutes.has("LOCAL"));
+    t.ok(seenRoutes.has("LIMITED"));
+    t.ok(seenRoutes.has("BABY BULLET"));
+
+    t.end();
+  });
+
+  test("sf->sj direction=sb", function(t) {
+    var getNextStop = getSchedule({
+      from: "ctsf",
+      to: "ctsj",
+      date: new Date(CURRENT_YEAR, 11, 1, 12),
+    });
+    for (var i = 0; i < 20; i++) {
+      var stop = getNextStop();
+      t.equal(stop.direction, "SB");
+    }
+    t.end();
+  });
+
+  test("sj->sf direction=nb", function(t) {
+    var getNextStop = getSchedule({
+      from: "ctsj",
+      to: "ctsf",
+      date: new Date(CURRENT_YEAR, 11, 1, 12),
+    });
+    for (var i = 0; i < 20; i++) {
+      var stop = getNextStop();
+      t.equal(stop.direction, "NB");
+    }
+    t.end();
+  });
+
+  test("get route from every station to every other station", function(t) {
+    var limitedStations = [
+      "ctat",
+      "ctbr",
+      "ctta",
+      "ctco",
+    ];
+    stations.forEach(function(fromStation) {
+      if (limitedStations.indexOf(fromStation.id) !== -1) {
+        return;
+      }
+      stations.forEach(function(toStation) {
+        if (limitedStations.indexOf(toStation.id) !== -1) {
+          return;
+        }
+        if (fromStation === toStation) {
+          return;
+        }
+        else {
+          var getNextStop = getSchedule({
+            from: fromStation.id,
+            to: toStation.id,
+            date: new Date(CURRENT_YEAR, 11, 1, 12),
+          });
+          var stop = getNextStop();
+          t.assert(stop, "route from " + fromStation.id + " to " + toStation.id);
+        }
+      });
+    });
     t.end();
   });
 });
